0e483d9b8d365746464ca39c67a2a8d9df97338e
Akshay6006 <akshaysingh09oct@gmail.com>
2026-08-18 17:27:54 +0530

    Check JWT environment

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
+console.log("JWT_SECRET loaded:", !!process.env.JWT_SECRET);
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
